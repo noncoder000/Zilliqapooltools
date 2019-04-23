692428ba936d6375d3dccca5fd529cf3e5cfb6d1
noncoder000 <32987453+noncoder000@users.noreply.github.com>
2019-04-23 10:10:48 -0400

    Add files via upload
    Download your keys and addr from your node using plink/putty with root password

---
+++ dladdr & dlkeys.bat
@@ -0,0 +1,7 @@
+
+pscp.exe -pw yourootpassword root@XXX.XXX.XXX.XXX:/root/Desktop/join/mykey.txt "C:\zilliqa\path\to\keys\mykeyX.txt"
+
+pscp.exe -pw yourootpassword root@XXX.XXX.XXX.XXX:/root/Desktop/join/myaddr.txt "C:\zilliqa\path\to\addr\myaddrX.txt"
+
+
+
