8c2e22abb057f9c9311f004b506f0f1448f74b81
noncoder000 <32987453+noncoder000@users.noreply.github.com>
2019-04-23 10:07:52 -0400

    Add files via upload
    Rebuild a node remotely using plink/putty using root password

---
+++ rebuild node.bat
@@ -0,0 +1,10 @@
+
+plink.exe -ssh root@XXX.XXX.XXX.XXX -pw yourootpassword docker stop zil 
+plink.exe -ssh root@XXX.XXX.XXX.XXX -pw yourootpassword rm -r /root/Desktop/join
+plink.exe -ssh root@XXX.XXX.XXX.XXX -pw yourootpassword mkdir /root/Desktop/join
+plink.exe -ssh root@XXX.XXX.XXX.XXX -pw yourootpassword wget https://mainnet-join.zilliqa.com/configuration.tar.gz -O /root/Desktop/join/configuration.tar.gz
+plink.exe -ssh root@XXX.XXX.XXX.XXX -pw yourootpassword tar zxvf /root/Desktop/join/configuration.tar.gz -C /root/Desktop/join/
+pscp.exe -r -pw yourootpassword% C:\zilliqa\path\to\keys\constantsX.xml root@XXX.XXX.XXX.XXX:"/root/Desktop/join/constants.xml"
+pscp.exe -r -pw yourootpassword C:\zilliqa\path\to\keys\mykeyXXX.txt root@XXX.XXX.XXX.XXX:"/root/Desktop/join/mykey.txt"
+pscp.exe -r -pw yourootpassword C:\zilliqa\path\to\keys\launch_docker.sh root@XXX.XXX.XXX.XXX:"/root/Desktop/join/launch_docker.sh"
+plink.exe -ssh root@XXX.XXX.XXX.XXX -pw yourootpassword sudo bash -x "/root/Desktop/join/launch_docker.sh"
